8f4d66cb181c6be3cc7067f0c91794d0ecf43079
Aggelos Margaritis <aggelos@margaritis.eu>
2021-01-24 12:27:38 +0200

    Improved user system

--- src/main/java/eu/margaritis/aggelos/projects/virtualschool/VirtualSchool.java
+++ src/main/java/eu/margaritis/aggelos/projects/virtualschool/VirtualSchool.java
@@ -7,10 +7,6 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 import eu.margaritis.aggelos.projects.virtualschool.proxy.CommonProxy;
 import eu.margaritis.aggelos.projects.virtualschool.util.Reference;
 import eu.margaritis.aggelos.projects.virtualschool.util.handlers.RegistryHandler;
--- src/main/java/eu/margaritis/aggelos/projects/virtualschool/users/User.java
+++ src/main/java/eu/margaritis/aggelos/projects/virtualschool/users/User.java
@@ -5,11 +5,15 @@ public class User {
 	private final String username;
 	private final String password;
 	private final Role role;
-	
-	public User(final String username, final String password, final Role role) {
+	private final String firstName;
+	private final String lastName;
+
+	public User(final String username, final String password, final Role role, final String firstName, final String lastName) {
 		this.username = username;
 		this.password = password;
 		this.role = role;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		UserUtil.getUsers().add(this);
 	}
 
@@ -24,5 +28,13 @@ public class User {
 	public final Role getRole() {
 		return role;
 	}
-	
+
+	public final String getFirstName() {
+		return firstName;
+	}
+
+	public final String getLastName() {
+		return lastName;
+	}
+
 }
