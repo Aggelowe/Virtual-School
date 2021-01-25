b9ca47ec27a50c7d1a6663391bb2848fd22338f2
Aggelos Margaritis <aggelos@margaritis.eu>
2021-01-25 18:56:35 +0200

    Fixed imports

--- src/main/java/eu/margaritis/aggelos/projects/virtualschool/events/voicechat/ServerVoiceChatStartEvent.java
+++ src/main/java/eu/margaritis/aggelos/projects/virtualschool/events/voicechat/ServerVoiceChatStartEvent.java
@@ -4,7 +4,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import eu.margaritis.aggelos.projects.virtualschool.util.Reference;
 
