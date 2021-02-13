27435baac52b2898abb94aa1fd620e5d3136de2f
Aggelos Margaritis <aggelos@margaritis.eu>
2021-02-13 14:53:41 +0200

    Update VoiceChatClient.java

--- src/main/java/eu/margaritis/aggelos/projects/virtualschool/voicechat/VoiceChatClient.java
+++ src/main/java/eu/margaritis/aggelos/projects/virtualschool/voicechat/VoiceChatClient.java
@@ -1,10 +1,17 @@
 package eu.margaritis.aggelos.projects.virtualschool.voicechat;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.SocketAddress;
+
+import org.apache.logging.log4j.Level;
 
 import eu.margaritis.aggelos.projects.virtualschool.events.voicechat.ClientVoiceChatJoinEvent;
 import eu.margaritis.aggelos.projects.virtualschool.events.voicechat.ClientVoiceChatLeaveEvent;
+import eu.margaritis.aggelos.projects.virtualschool.util.Reference;
+import eu.margaritis.aggelos.projects.virtualschool.util.exceptions.ObjectNotFoundException;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -22,7 +29,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public final class VoiceChatClient {
 
-	private static final Thread AUDIO_CAPTURE_THREAD = new Thread(new AudioCaptureThread(), "Audio Capture Thread");
+	private static BufferedOutputStream output;
+	private static BufferedInputStream input;
 
 	/**
 	 * This method which must be called only by the
@@ -32,7 +40,16 @@ public final class VoiceChatClient {
 	 * @see ClientVoiceChatJoinEvent#voiceChatClientJoin(PlayerLoggedInEvent)
 	 */
 	public static void start() {
-
+		try {
+			if (getMinecraftHostAdress() == null) {
+				return;
+			}
+			Socket socket = new Socket(getMinecraftHostAdress().getHostString(), getMinecraftHostAdress().getPort());
+			output = new BufferedOutputStream(socket.getOutputStream());
+			input = new BufferedInputStream(socket.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -46,18 +63,15 @@ public final class VoiceChatClient {
 
 	}
 
-	private static InetSocketAddress getMinecraftHostAdress() {
-		if(Minecraft.getMinecraft().isSingleplayer()) {
-			return null;
-		}
-		if(Minecraft.getMinecraft().isIntegratedServerRunning()) {
+	private static InetSocketAddress getMinecraftHostAdress() throws ObjectNotFoundException {
+		if (Minecraft.getMinecraft().isIntegratedServerRunning()) {
 			return new InetSocketAddress("localhost", Minecraft.getMinecraft().getIntegratedServer().getServerPort());
 		}
 		SocketAddress rawAdress = Minecraft.getMinecraft().getConnection().getNetworkManager().getRemoteAddress();
 		if (rawAdress instanceof InetSocketAddress) {
 			return (InetSocketAddress) rawAdress;
 		}
-		return null;
+		throw new ObjectNotFoundException("Could not get the remote host adress.");
 	}
 
 	/**
@@ -69,11 +83,11 @@ public final class VoiceChatClient {
 	 * @author Aggelos
 	 *
 	 */
-	private static final class AudioCaptureThread implements Runnable {
+	private static final class AudioCaptureThread extends Thread {
 
 		@Override
 		public void run() {
-			return;
+
 		}
 
 	}
